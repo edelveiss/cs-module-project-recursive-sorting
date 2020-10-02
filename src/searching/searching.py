f1e5a2421f2ca83c228f06149e15764710ab08cb
Tatiana Zhizhimontova <tatyana.zhi@gmail.com>
2020-10-01 18:24:23 -0700

    MVP and stretch goal are completed

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,20 @@
 # TO-DO: Implement a recursive implementation of binary search
+#the worst case is O(n), the average case is log(n)
 def binary_search(arr, target, start, end):
-    # Your code here
+    middle = (start + end) // 2
+    if start > end:
+        return -1
+    if arr[middle] == target:
+        return middle
+    if arr[middle] > target:
+        return binary_search(arr, target, start, (middle -1) )
+    else:
+        return binary_search(arr, target,  (middle + 1), end)
 
+# arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
+# asc1 = [2, 4, 12, 14, 17, 30, 46, 47, 51, 54, 61]
+# print(binary_search(asc1, 12, 0, len(asc1) - 1))
+# print(binary_search(arr1, 9, 0, len(arr1) - 1))
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
@@ -9,6 +22,64 @@ def binary_search(arr, target, start, end):
 # sorted in ascending order or in descending order
 # You can implement this function either recursively 
 # or iteratively
+
 def agnostic_binary_search(arr, target):
-    # Your code here
+    ascending = 0
+    if arr[0] < arr[len(arr)-1]:
+        ascending = 1 
+    start = 0
+    end = len(arr) - 1
+    found = -1
+
+    while start <= end and found < 0:
+        middle = (start + end) // 2
+        if arr[middle] == target:
+            found = middle
+            return found
+        if ascending == 1:
+            if arr[middle] > target:
+                end = middle -1
+            if arr[middle] < target: 
+                start = middle +1
+        else:
+            if arr[middle] > target:
+                start = middle +1
+            if arr[middle] < target: 
+                end = middle -1
+
+    return found 
+
+#----------------recursively-------------------------
+def agnostic_binary_search1(arr, target,start, end):
+    
+    ascending = 0
+    if arr[0] < arr[len(arr)-1]:
+        ascending = 1 
+    middle = (start + end) // 2
+    if start > end:
+        return -1
+    if arr[middle] == target:
+        return middle
+
+    if ascending == 1:
+        if arr[middle] > target:
+            return agnostic_binary_search1(arr, target, start, middle-1)
+        else:
+            return agnostic_binary_search1(arr, target,middle+1, end)
+    else:
+        if arr[middle] > target:
+            return agnostic_binary_search1(arr, target, middle +1, end)
+        else:
+            return agnostic_binary_search1(arr, target, start, middle-1)
+
+ascending = [2, 4, 12, 14, 17, 30, 46, 47, 51, 54, 61]
+descending = [101, 98, 57, 49, 45, 13, -3, -17, -61]
+print(agnostic_binary_search1(ascending, 12, 0, len(ascending) - 1))
+print(agnostic_binary_search1(descending, 49, 0, len(descending) - 1))
+    
+
+
+    
+    
+    
 
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -2,27 +2,99 @@
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
-
-    # Your code here
-
+    i=j=m=0
+   
+    while i < len(arrA) and j < len(arrB):
+        if arrA[i] < arrB[j]:
+            merged_arr[m] = arrA[i]
+            i += 1
+        else:
+            merged_arr[m] = arrB[j]
+            j += 1
+        m += 1
+    while i < len(arrA):
+        merged_arr[m] = arrA[i]
+        i += 1
+        m += 1
+    while j < len(arrB):
+        merged_arr[m] = arrB[j]
+        j += 1
+        m += 1
 
     return merged_arr
 
+arr1 = [1,4,7,9]
+arr2 = [2,5,8,10,12,20,25]
+
 # TO-DO: implement the Merge Sort function below recursively
-def merge_sort(arr):
-    # Your code here
-
-
-    return arr
+def merge_sort(arr): #O(nlog(n))
+    if len(arr) <= 1:
+        return arr
+   
+    left = merge_sort( arr[:len(arr) // 2] )
+    right= merge_sort(arr[len(arr) // 2:])
+    return merge(left, right)
+    
+arr10 = [4,6,9,7,5]
+print(merge_sort(arr10))
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists 
 # or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
-    # Your code here
 
+def merge_in_place(arr, start, mid, end):
+    left = arr[start:mid +1]
+    right = arr[mid +1:end +1]
+    i = j = 0
+    m = start
+    for index in range(m, end +1):
+        if j >= len(right) or (i < len(left) and left[i] < right[j]):
+            arr[index] = left[i]
+            i +=1
+        else:
+            arr[index] = right[j]
+            j +=1
 
 def merge_sort_in_place(arr, l, r):
-    # Your code here
+    if r - l > 0:
+        middle = (l + r) // 2
+        merge_sort_in_place(arr, l, middle)
+        merge_sort_in_place(arr,middle+1,r)
+        merge_in_place(arr,l,middle,r)
+
+arr3 = [2,5,8,10,12,20,25]
+arr10 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+merge_sort_in_place(arr10,0,len(arr10)-1)
+print(arr10)
+
+merge_sort_in_place(arr3,0,len(arr3)-1)
+print(arr3)
+
+
+
+
+# def merge_in_place(arr, start, mid, end):
+#     left = arr[start:mid]
+#     right = arr[mid:end]
+#     i = j = 0
+#     m = start
+#     for index in range(m, end):
+#         if j >= len(right) or (i < len(left) and left[i] < right[j]):
+#             arr[index] = left[i]
+#             i +=1
+#         else:
+#             arr[index] = right[j]
+#             j +=1
+
+# def merge_sort_in_place(arr, l, r):
+#     if r - l > 1:
+#         middle = (l + r) // 2
+#         merge_sort_in_place(arr, l, middle)
+#         merge_sort_in_place(arr,middle,r)
+#         merge_in_place(arr,l,middle,r)
+
+# arr111 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+# merge_sort_in_place(arr111,0,len(arr111))
+# print(arr111)
 
